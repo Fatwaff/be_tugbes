cc7f8eab65e6990a9804ad0e6b91216f050d16ce
Fatwa <fax.mp4@gmail.com>
2023-04-27 17:11:09 +0700

    update komen

--- dev_test.go
+++ dev_test.go
@@ -8,15 +8,17 @@ import (
 	"github.com/Fatwaff/be_tugbes/module"
 )
 
-func TestInserUser(t *testing.T) {
-	var data model.User
-	data.FirstName = "Naufal Dekha"
-	data.LastName = "Widana"
-	data.Email = "nopal@gmail.com"
-	data.Password = "jklbnm"
-	hasil := module.InsertOneDoc(module.MongoConnect(), "user", data)
-	fmt.Println(hasil)
-}
+// func TestInserOneDoc(t *testing.T) {
+// 	var data model.User
+// 	data.FirstName = "Fatwa Fatahillah"
+// 	data.LastName = "Fatah"
+// 	data.Email = "fatwa@gmail.com"
+// 	data.Password = "jklbnm"
+// 	hasil := module.InsertOneDoc(module.MongoConnect(), "user", data)
+// 	fmt.Println(hasil)
+// }
+
+// Batas suci
 
 func TestGetUserFromEmail(t *testing.T) {
 	email := "dimas@gmail.com"
@@ -30,36 +32,16 @@ func TestGetAllDataUser(t *testing.T) {
 	fmt.Println(data)
 }
 
-// func TestInserUser(t *testing.T) {
-// 	data := bson.M{
-// 		"firstname":    "Naufal Dehka",
-// 		"lastname":     "Widana",
-// 		"email":     	"nopal@gmail.com",
-// 		"password": 	"jklbnm",
-// 	}
-// 	hasil, err := module.InsertUser(module.MongoConnect(), "user", data)
-// 	if err != nil {
-// 		t.Errorf("Error inserting data: %v", err)
-// 	}
-// 	fmt.Println("Data berhasil disimpan dengan id :", hasil.Hex())
-// }
+ func TestInserUser(t *testing.T) {
+ 	var data model.User
+	data.FirstName = "Naufal Dekha"
+	data.LastName = "Widana"
+	data.Email = "nopal@gmail.com"
+	data.Password = "jklbnm"
+ 	hasil, err := module.InsertOneDoc(module.MongoConnect(), "user", data)
+ 	if err != nil {
+ 		t.Errorf("Error inserting data: %v", err)
+ 	}
+ 	fmt.Println("Data berhasil disimpan dengan id :", hasil.Hex())
+}
 
-// func TestInserUser(t *testing.T) {
-// 	firstname := "Fatwa Fatahillah"
-// 	lastname := "Fatah"
-// 	email := "fatwa@gmail.com"
-// 	password := "qwerty"
-// 	hasil := module.InsertUser(module.MongoConnect(), "user", firstname, lastname, email, password)
-// 	fmt.Println(hasil)
-// }
-
-// func TestGetAllPresensiFromKehadiran(t *testing.T) {
-// 	kehadiran := "masuk"
-// 	data := module.GetAllPresensiFromKehadiran(kehadiran, module.MongoConn, "presensi")
-// 	fmt.Println(data)
-// }
-
-// func TestGetAllMataKuliah(t *testing.T) {
-// 	data := module.GetAllMataKuliah(module.MongoConn, "matkul")
-// 	fmt.Println(data)
-// }
